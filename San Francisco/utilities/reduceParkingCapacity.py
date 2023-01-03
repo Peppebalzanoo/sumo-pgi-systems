0ae7f36eea3225e06b1695785a996fcc69171b5e
Peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2023-01-03 12:47:02 +0100

    Code refactoring for reduceParkingCapacity.py

--- San Francisco/utilities/reduceParkingCapacity.py
+++ San Francisco/utilities/reduceParkingCapacity.py
@@ -1,10 +1,10 @@
-import xml.etree.ElementTree as ElementTree
+import xml.etree.ElementTree as ET
 import math
 
 def reduce_to(percentage):
-    tree = ElementTree.parse('../parking_capacity_100%/on_parking_100%.add.xml')
+    tree = ET.parse('../parking_capacity_100%/on_parking_100%.add.xml')
     root = tree.getroot()
-    new_root = ElementTree.Element(root.tag)
+    new_root = ET.Element(root.tag)
     for elem in root.findall(".//parkingArea"):
         temp_attr_dictionary = elem.attrib
 
@@ -15,13 +15,13 @@ def reduce_to(percentage):
         temp_attr_dictionary["roadsideCapacity"] = str(new_capacity)
         new_root.append(elem)
 
-    new_tree = ElementTree.ElementTree(new_root)
+    new_tree = ET.ElementTree(new_root)
     new_tree.write("../parking_capacity_"+str(percentage)+"%/on_parking_"+str(percentage)+"%.add.xml", encoding="utf-8", xml_declaration=True)
 
 
-    tree = ElementTree.parse('../parking_capacity_100%/off_parking_100%.add.xml')
+    tree = ET.parse('../parking_capacity_100%/off_parking_100%.add.xml')
     root = tree.getroot()
-    new_root = ElementTree.Element(root.tag)
+    new_root = ET.Element(root.tag)
     for elem in root.findall(".//parkingArea"):
         temp_attr_dictionary = elem.attrib
 
@@ -32,7 +32,7 @@ def reduce_to(percentage):
         temp_attr_dictionary["roadsideCapacity"] = str(new_capacity)
         new_root.append(elem)
 
-    new_tree = ElementTree.ElementTree(new_root)
+    new_tree = ET.ElementTree(new_root)
     new_tree.write("../parking_capacity_"+str(percentage)+"%/off_parking_"+str(percentage)+"%.add.xml", encoding="utf-8", xml_declaration=True)
 
 
