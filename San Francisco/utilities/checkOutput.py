18257f781272f94e3964b965d94efb0d67d6e342
Peppebalzanoo <giuseppe.balzano7@studenti.unina.it>
2023-01-11 15:58:34 +0100

    Results of scenarios 50%

--- San Francisco/utilities/checkOutput.py
+++ San Francisco/utilities/checkOutput.py
@@ -123,15 +123,15 @@ def run(strategia, scenario):
 def main():
 
     run("strategia1", "100%")
-    # run("strategia1", "75%")
+    run("strategia1", "75%")
     # run("strategia1", "50%")
 
     run("strategia2", "100%")
-    # run("strategia2", "75%")
+    run("strategia2", "75%")
     # run("strategia2", "50%")
 
     run("strategia3", "100%")
-    # run("strategia3", "75%")
+    run("strategia3", "75%")
     # run("strategia3", "50%")
 
     # get_num_parking_for_edge()
